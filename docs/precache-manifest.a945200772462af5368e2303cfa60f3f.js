6e3e77ff10c38813cb0fa15de49633f2aa1a9578
Janina <janinatalvarez@Gmail.com>
2020-10-13 15:10:26 -0700

    build project

---
+++ docs/precache-manifest.a945200772462af5368e2303cfa60f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d668e420c6adfe369b48a2943f9b8c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ddac7460440bf6c8b23",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5b104c73ae42d6cb28d",
+    "url": "./static/js/2.29eb80a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.29eb80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddac7460440bf6c8b23",
+    "url": "./static/js/main.83b900ab.chunk.js"
+  },
+  {
+    "revision": "bb30c7d0fbfa6286a3a5",
+    "url": "./static/js/runtime-main.4e496788.js"
+  }
+]);
