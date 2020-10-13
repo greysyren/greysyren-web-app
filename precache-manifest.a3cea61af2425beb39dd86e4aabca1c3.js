9f3df297702d5c62418446a9e07b4ed61bbf0da8
Janina <janinatalvarez@Gmail.com>
2020-10-13 15:28:32 -0700

    Updates

---
+++ precache-manifest.a3cea61af2425beb39dd86e4aabca1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361e42266bd68a4f3004de3b9c267959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a68699ad5f9bb90bfc52",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5b104c73ae42d6cb28d",
+    "url": "/static/js/2.29eb80a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29eb80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68699ad5f9bb90bfc52",
+    "url": "/static/js/main.906e01c8.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/greysyren-web-app/precache-manifest.2445459736ad1bc80d5de66ef8b4aeb4.js"
+  "/precache-manifest.a3cea61af2425beb39dd86e4aabca1c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/greysyren-web-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
